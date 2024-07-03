acd34d58c5928979f16b98e7941a31783b3cace8
deriz13 <dederizki30102@gmail.com>
2024-07-03 19:53:50 +0700

    fix script

--- resources/views/Admin/layouts/app.blade.php
+++ resources/views/Admin/layouts/app.blade.php
@@ -4,18 +4,18 @@
 <head>
     
     <!-- Favicons -->
-    <link rel="apple-touch-icon" href="../../assets/img/favicon/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="../../assets/img/favicon/favicon-32x32.png" sizes="32x32" type="image/png">
-    <link rel="icon" href="../../assets/img/favicon/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="apple-touch-icon" href=".{{ url('/assets/img/favicon/apple-touch-icon.png') }}" sizes="180x180">
+    <link rel="icon" href="{{ url('/assets/img/favicon/favicon-32x32.png') }}" sizes="32x32" type="image/png">
+    <link rel="icon" href="{{ url('/assets/img/favicon/favicon-16x16.png') }}" sizes="16x16" type="image/png">
 
-    <link rel="mask-icon" href="../../assets/img/favicon/safari-pinned-tab.svg" color="#563d7c">
-    <link rel="icon" href="../../assets/img/favicon/favicon.ico">
-    <meta name="msapplication-config" content="../../assets/img/favicons/browserconfig.xml">
+    <link rel="mask-icon" href="{{ url('/assets/img/favicon/safari-pinned-tab.svg') }}" color="#563d7c">
+    <link rel="icon" href="{{ url('/assets/img/favicon/favicon.ico') }}">
+    <meta name="msapplication-config" content="{{ url('/assets/img/favicons/browserconfig.xml') }}">
     <meta name="theme-color" content="#563d7c">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
     
     <!-- Apex Charts -->
-    <link type="text/css" href="/vendor/apexcharts/apexcharts.css" rel="stylesheet">
+    <link type="text/css" href="{{ url('/vendor/apexcharts/apexcharts.css') }}" rel="stylesheet">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <!-- Datepicker -->
@@ -23,14 +23,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.1.4/dist/css/datepicker-bs4.min.css">
 
     <!-- Fontawesome -->
-    <link type="text/css" href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link type="text/css" href="{{ url('/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     
     
     <!-- Notyf -->
-    <link type="text/css" href="/vendor/notyf/notyf.min.css" rel="stylesheet">
+    <link type="text/css" href="{{ url('/vendor/notyf/notyf.min.css') }}" rel="stylesheet">
     
     <!-- Volt CSS -->
-    <link type="text/css" href="/css/volt.css" rel="stylesheet">
+    <link type="text/css" href="{{ url('/css/volt.css') }}" rel="stylesheet">
 
     <!-- Livewire Styles -->
 <style >
@@ -58,75 +58,28 @@
     @keyframes livewireautofill { from {} }
 </style>
 
-    <!-- Livewire Scripts -->
 
-<script src="/livewire/livewire.js?id=90730a3b0e7144480175" data-turbo-eval="false" data-turbolinks-eval="false" ></script>
-<script data-turbo-eval="false" data-turbolinks-eval="false" >
-    if (window.livewire) {
-	    console.warn('Livewire: It looks like Livewire\'s @livewireScripts JavaScript assets have already been loaded. Make sure you aren\'t loading them twice.')
-	}
 
-    window.livewire = new Livewire();
-    window.livewire.devTools(true);
-    window.Livewire = window.livewire;
-    window.livewire_app_url = '';
-    window.livewire_token = 'BczWqut5ir0RY86s9ffVQ8EfnmShAoALr8OzZsxo';
-
-	/* Make sure Livewire loads first. */
-	if (window.Alpine) {
-	    /* Defer showing the warning so it doesn't get buried under downstream errors. */
-	    document.addEventListener("DOMContentLoaded", function () {
-	        setTimeout(function() {
-	            console.warn("Livewire: It looks like AlpineJS has already been loaded. Make sure Livewire\'s scripts are loaded before Alpine.\\n\\n Reference docs for more info: http://laravel-livewire.com/docs/alpine-js")
-	        })
-	    });
-	}
-
-	/* Make Alpine wait until Livewire is finished rendering to do its thing. */
-    window.deferLoadingAlpine = function (callback) {
-        window.addEventListener('livewire:load', function () {
-            callback();
-        });
-    };
-
-    let started = false;
-
-    window.addEventListener('alpine:initializing', function () {
-        if (! started) {
-            window.livewire.start();
-
-            started = true;
-        }
-    });
-
-    document.addEventListener("DOMContentLoaded", function () {
-        if (! started) {
-            window.livewire.start();
-
-            started = true;
-        }
-    });
-</script>
 
     <!-- Core -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 
     <!-- Vendor JS -->
-    <script src="/assets/js/on-screen.umd.min.js"></script>
+    <script src="{{ url('assets/js/on-screen.umd.min.js') }}"></script>
 
     <!-- Slider -->
-    <script src="/assets/js/nouislider.min.js"></script>
+    <script src="{{ url('assets/js/nouislider.min.js') }}"></script>
 
     <!-- Smooth scroll -->
-    <script src="/assets/js/smooth-scroll.polyfills.min.js"></script>
+    <script src="{{ url('assets/js/smooth-scroll.polyfills.min.js') }}"></script>
 
     <!-- Apex Charts -->
-    <script src="/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="{{ url('vendor/apexcharts/apexcharts.min.js') }}"></script>
 
     <!-- Charts -->
-    <script src="/assets/js/chartist.min.js"></script>
-    <script src="/assets/js/chartist-plugin-tooltip.min.js"></script>
+    <script src="{{ url('assets/js/chartist.min.js') }}"></script>
+    <script src="{{ url('assets/js/chartist-plugin-tooltip.min.js') }}"></script>
 
     <!-- Datepicker -->
     <script src="https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.1.4/dist/js/datepicker.min.js"></script>
@@ -134,22 +87,23 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
 
     <!-- Notyf -->
-    <script src="/vendor/notyf/notyf.min.js"></script>
+    <script src="{{ url('vendor/notyf/notyf.min.js') }}"></script>
 
     <!-- Simplebar -->
-    <script src="/assets/js/simplebar.min.js"></script>
+    <script src="{{ url('assets/js/simplebar.min.js') }}"></script>
 
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     
     <!-- Volt JS -->
-    <script src="/assets/js/volt.js"></script>
+    <script src="{{ url('assets/js/volt.js') }}"></script>
 
 </head>
 
 <body>
        
     @include('admin.layouts.sidebar')
+    
     <main class="content">
         @include('admin.layouts.header')
 
