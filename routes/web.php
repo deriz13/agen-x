119a7cc52387d8e3659e7c818189fcb27f652827
deriz13 <dederizki30102@gmail.com>
2024-07-03 17:35:03 +0700

    implement page admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CheckinController;
+use App\Http\Controllers\LaporanController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -12,3 +16,7 @@ Route::post('create/ticket', [HomeController::class, 'creteTicket'])->name('tick
 Route::get('ticket/{ticket_id}', [HomeController::class, 'showTicket'])->name('home.show_ticket');
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'loginStore'])->name('login.store');
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('pesanan', [OrderController::class, 'index'])->name('order.index');
+Route::get('check-in', [CheckinController::class, 'index'])->name('checkin.index');
+Route::get('laporan', [LaporanController::class, 'index'])->name('report.index');
